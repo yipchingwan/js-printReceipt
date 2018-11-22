76d035d24ddf255f80d4421258369f145f728f78
KYLE\Surface <kyle.public10415@gmail.com>
2018-11-23 02:52:17 +0800

    modified function following tasking diagram

--- main.js
+++ main.js
@@ -1,57 +1,98 @@
-function printReceipt (barCodeInCar) {
-    var groupedItem={};
-    var allItems = loadAllItems();
-    var promotionsItems = loadPromotions();
-    var filteredItems;
-    var result;
-    var total=0;
-    var totalBeforePromotion=0;
-    //group item
-    barCodeInCar.filter(items=>{
-        groupedItem[items] = 0;
-    });
-    barCodeInCar.filter(items=>{
-        let splitedItem=[];
-        splitedItem = items.split('-');
-        if(splitedItem.length==1){
-            groupedItem[items]++;
-        }
-        else{
-            if(groupedItem[splitedItem[0]]==null){
-                groupedItem[splitedItem[0]] = groupedItem[items];
-            }
-            
-            groupedItem[splitedItem[0]] += parseFloat(splitedItem[1]);
-            delete groupedItem[items];
+function printReceipt (barCodeInCar) {    
+    const barCodeWithCount= groupItem(barCodeInCar);
+    const filtereditemsWithCount = addCountToFilterItems(barCodeWithCount, loadAllItems);
+    const itemWithCountAndSubTotal = addSubTotalToItems(filtereditemsWithCount,loadPromotions, "BUY_TWO_GET_ONE_FREE");
+    const totalAndSaving = calTotalAndSaving(itemWithCountAndSubTotal);
+    const receipt = formatReceipt(itemWithCountAndSubTotal, totalAndSaving);
+    return receipt;
+}
 
-        }     
+function groupItem(barCodeInCar){
+	  // make object to store items with count
+    var itemWithCount = {}
+    barCodeInCar.filter((item,i)=>barCodeInCar.indexOf(item)===i).map(item=>{
+    		itemWithCount[item] = barCodeInCar.filter(x=>item===x).length;
     });
-    //filter item from allitems
-    filteredItems= allItems.filter(item=>{
-        if(item.barcode in groupedItem){
-            return item;
-        }
-    });
-    //map count to filtered items
-    filteredItems.map(items=>items.count = groupedItem[items.barcode]);
 
-    //map Promotion to filtered items
-    filteredItems.map(items=>{
-        if(promotionsItems[0].barcodes.includes(items.barcode)){
-            if(items.count>=2){
-                items.realCount = items.count-parseInt(items.count/3);
-            }
-            else{
-                items.realCount = items.count;
-            }
-        }
-        else{
-            items.realCount = items.count;
-        }
+    //Find out the items with '-', and change there count
+    Object.keys(itemWithCount).forEach(barcode=>{
+    	var splitedItem = barcode.split('-');
+    	if(splitedItem.length>1){
+    		if(itemWithCount[splitedItem[0]]==null){
+    			itemWithCount[splitedItem[0]] = itemWithCount[barcode];
+    			itemWithCount[splitedItem[0]] = parseFloat(splitedItem[1]);
+    		}
+    		else{
+    			itemWithCount[splitedItem[0]] += parseFloat(splitedItem[1]);
+    		}
+    		delete itemWithCount[barcode];
+    	}
     });
-    //using filtered items to format string
-    result = '***<store earning no money>Receipt ***\n';
-    filteredItems.forEach(items=>{
+    return itemWithCount
+}
+
+function addCountToFilterItems(barCodeWithCount, loadAllItemsFn){	
+	const filteredItems = filterItemsFromAll(barCodeWithCount,loadAllItemsFn); 
+	return filteredItems.map(items=>{
+			items.count = barCodeWithCount[items.barcode];
+			return items;
+	});
+}
+
+function filterItemsFromAll(barCodeWithCount,loadAllItemsFn){
+	 return loadAllItemsFn().filter(item=>item.barcode in barCodeWithCount);
+}
+
+function addSubTotalToItems(filtereditemsWithCount, loadPromotionsFn, promotionType){
+	 const subTotal = calSubTotalWithPromotion(filtereditemsWithCount, loadPromotionsFn);
+	 return filtereditemsWithCount.map((items, index)=>{
+	 		items.subTotal = subTotal[index];
+	 		return items;
+	 })
+}
+
+function calSubTotalWithPromotion(filtereditemsWithCount, loadPromotionsFn, promotionType){ 
+		var subTotal = []
+		var selectedPromotion = {}
+		loadPromotionsFn().forEach(promotion=>{
+			if(promotion.type==="BUY_TWO_GET_ONE_FREE"){
+				selectedPromotion = promotion;
+				filtereditemsWithCount.forEach(items=>{
+					if(selectedPromotion.barcodes.includes(items.barcode)){
+						if(items.count>2){
+							subTotal.push(items.price*(items.count-parseInt(items.count/3)))
+						}
+						else{
+							subTotal.push(items.price*items.count)
+						}
+					}
+					else{
+						subTotal.push(items.price*items.count)
+					}
+				})
+
+			}
+		});
+
+		return subTotal
+}
+
+function calTotalAndSaving(itemWithCountAndSubTotal){
+	var totalAndSaving= {};
+	var totalBeforePromotion = 0;
+	totalAndSaving.total = 0;
+	totalAndSaving.saving = 0;
+	itemWithCountAndSubTotal.forEach(item=>{
+		totalAndSaving.total += item.subTotal;
+		totalBeforePromotion += item.count*item.price;
+	});
+	totalAndSaving.saving = totalBeforePromotion - totalAndSaving.total;
+	return totalAndSaving;
+}
+
+function formatReceipt(itemWithCountAndSubTotal, totalAndSaving){
+	result = '***<store earning no money>Receipt ***\n';
+    itemWithCountAndSubTotal.forEach(items=>{
         var newUnit;
         if(!(items.unit==='kg')){
             if(items.count>1){
@@ -60,25 +101,14 @@ function printReceipt (barCodeInCar) {
             else{
                 newUnit = items.unit;
             }
-            result += 'Name: '+items.name+', Quantity: '+items.count+' '+newUnit+', Unit price: '+items.price.toFixed(2)+' (yuan), Subtotal: '+(items.realCount*items.price).toFixed(2)+' (yuan)\n';
+            result += 'Name: '+items.name+', Quantity: '+items.count+' '+newUnit+', Unit price: '+items.price.toFixed(2)+' (yuan), Subtotal: '+items.subTotal.toFixed(2)+' (yuan)\n';
         }
         else{
-            result += 'Name: '+items.name+', Quantity: '+items.count+' '+items.unit+', Unit price: '+items.price.toFixed(2)+' (yuan), Subtotal: '+(items.realCount*items.price).toFixed(2)+' (yuan)\n';
+            result += 'Name: '+items.name+', Quantity: '+items.count+' '+items.unit+', Unit price: '+items.price.toFixed(2)+' (yuan), Subtotal: '+items.subTotal.toFixed(2)+' (yuan)\n';
         }
-        total+=items.realCount*items.price;
-        totalBeforePromotion+=items.count*items.price;
-
     });
-    result +='----------------------\n' + 'Total: '+total.toFixed(2)+' (yuan)\n' + 'Saving: '+(totalBeforePromotion-total).toFixed(2)+' (yuan)\n'+'**********************';
+    result +='----------------------\n' + 'Total: '+totalAndSaving.total.toFixed(2)+' (yuan)\n' + 'Saving: '+totalAndSaving.saving.toFixed(2)+' (yuan)\n'+'**********************';
     return result;
-}
-
-function CalulateSubTotal(){
-    
-}
-
-function groupItem(){
-
 }
 
 function loadAllItems() {
